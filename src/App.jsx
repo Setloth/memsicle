4d3e279554a528130d09deea07eb87fbaa506dab
Seth <44213317+Sploric@users.noreply.github.com>
2022-10-27 18:05:13 +0000

    set

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css'
 
+import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material"
+import { Menu as MenuIcon } from '@mui/icons-material';
+
 export default function App() {
-  return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
