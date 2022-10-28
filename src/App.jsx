8cf12a7c1daf9d7713621d8725f3dfa9be6f2d74
Seth <44213317+Sploric@users.noreply.github.com>
2022-10-28 19:17:20 +0000

    frontend updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,58 @@
 import './App.css'
-
-import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material"
+import { useEffect, useState } from "react"
+import { Box, Badge, Menu, MenuItem, Avatar, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material"
 import { Menu as MenuIcon } from '@mui/icons-material';
 
 export default function App() {
+
+    const [user, setUser] = useState(null)
+    const [menuAnchor, setMenuAnchor] = useState(null)
+
+
+    useEffect(() => {
+        getUserInfo().then(data => {
+            setUser(data)
+        })
+
+    }, [])
+
+
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
-                        size="large"
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{ mr: 2 }}
+                        sx={{ mr: 0.5, my:1 }}
                     >
-                        <MenuIcon />
+                        <Avatar sx={{ width: 48, height: 48 }} src="/favicon.png"/>
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                        Memsicle
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    {
+                        !user
+                            ?
+                            <Button onClick={LoginWithReplit} color="inherit">Login</Button>
+                            :
+                            <IconButton onClick={(e) => {
+                                setMenuAnchor(e.target)
+                            }}><Avatar alt={user.name} src={user.profileImage}/></IconButton>
+                    }
+
+                    <Menu
+                        anchorEl={menuAnchor}
+                        open={!!menuAnchor}
+                        onClose={() => {
+                            setMenuAnchor(null)
+
+                        }}
+                    >
+                        <MenuItem>Logout</MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </Box>
